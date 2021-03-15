988f2f22fa0ca89070c2c26fec64ed64cc4dc6be
Bulut Fıçıcı <bulucyus@gmail.com>
2021-03-16 02:45:44 +0300

    added pairs

--- commands.py
+++ commands.py
@@ -18,27 +18,50 @@ def start_command(update, context):
 
 def wrong_command(update, context):
     
-    message = "Hey come on gimme a break ! Just use /start for now..."
+    message = "Hey come on gimme a break ! Just use /start /news and /market for now..."
     
     return update.message.reply_text(message)
 
 def get_news(update, context):
     
-    link="https://www.haberler.com/son-dakika/"
+    link="https://www.coinkolik.com/kripto-para-haberleri/"  
     
     r=requests.get(link)
     soup=BeautifulSoup(r.content, "lxml")
-
-    news=soup.find_all('div', attrs={'class': "hblnBox"})
     
-    for new in news[:5]:
-        
-        time=new.find('div', attrs={'class': "hblnTime"}).text
-        title=new.find("a", attrs={"class": "hblnTitle"}).get("title")   # başlığı çek
-        link=new.find("a", attrs={"class": "hblnTitle"}).get("href")     # linki çek
-        
+    news=soup.find_all("article")
     
-        my_news="Time    : {}\n\nTitle   : {}\n\nLink    : {}\n\n".format(time,title,link)
-        
+    for new in news[:]:
+    
+        link=new.find('a').get("href")
+        text=new.find("p").text
+        title=new.find("h2").text
+    
+        my_news="{}\n\n{}\n\nLink  :   {}\n\n".format(title,text,link)
+    
         update.message.reply_text(my_news)
+
+        
+def get_coins(update, context):
+    
+    link="https://www.coingecko.com/tr?utm_source=www.coinkolik.com&utm_medium=coin_converter_widget&utm_content=bitcoin"
+
+    r=requests.get(link)
+    soup=BeautifulSoup(r.content, "lxml")
+    
+    coins=soup.find_all("tr")
+    
+    for coin in coins[0:20]:
+    
+        middle=coin.find_all('td', attrs={"class": "td-price price text-right"})
+        
+        for x in middle[:]:
+            
+            span=x.find("span").get("data-coin-symbol").upper()
+            price=x.get("data-sort")
+            
+            market="{:<5}: {} $".format(span,price[0:6])
+            
+        
+            update.message.reply_text(market)
 
--- main.py
+++ main.py
@@ -12,8 +12,12 @@ def main():
     
     # start command
     dp.add_handler(CommandHandler("start", c.start_command))
+    
     # news
     dp.add_handler(CommandHandler("news", c.get_news))
+    
+    # news
+    dp.add_handler(CommandHandler("market", c.get_coins))
         
     #wrong command
     dp.add_handler(MessageHandler(Filters.text, c.wrong_command))
